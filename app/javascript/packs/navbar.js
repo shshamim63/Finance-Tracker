af3075549be2c6d28176898a1cdc5bc55fa4518a
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-06-12 18:44:07 +0600

    :lipstick: Finalized initial template
    Contains sidebar menu

---
+++ app/javascript/packs/navbar.js
@@ -0,0 +1,19 @@
+jQuery(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+});
