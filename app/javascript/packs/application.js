9b5ad393f82a7d5876f2817225cfae837cf07b99
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-06-13 21:47:22 +0600

    :ambulance: Fixed importing issue
    Moved import bootstrap down of fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,5 @@ require('./navbar')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import 'bootstrap'
 import "@fortawesome/fontawesome-free/js/all";
+import 'bootstrap';
