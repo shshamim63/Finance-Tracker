2f77c4b5140cd20a58b1d6e8454f4220871ec51f
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-11-09 18:04:00 +0600

    Added a method to check user is able to add stock and wether the stock already added or not.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+
+  def stock_already_added?(stock_symbol)
+    stock = Stock.find_by_ticker(stock_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exist?
+  end
+
+  def under_stock_limit?
+    (user_stocks.count < 2)
+  end
+
+  def can_add_stock?(stock_symbol)
+    under_stock_limit? && !stock_already_added?(stock_symbol)
+  end
 end
