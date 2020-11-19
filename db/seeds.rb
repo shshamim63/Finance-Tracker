09d94f4c3d21f6246a346c2931b11c039a9bdbd6
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-11-19 22:27:48 +0600

    Added buttons for different friendship actions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  first_name: 'Bruce',
+  last_name: 'Wayne',
+  username: 'Batman',
+  email: 'batman@gmail.com',
+  password: '123456'
+)
+
+User.create(
+  first_name: 'Dick',
+  last_name: 'Grayson',
+  username: 'Robin',
+  email: 'robin@gmail.com',
+  password: '123456'
+)
+
+User.create(
+  first_name: 'Ben',
+  last_name: 'Affleck',
+  username: 'Suicide Squad',
+  email: 'suicide.squad@gmail.com',
+  password: '123456'
+)
+
