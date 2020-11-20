67f764e5ef490edfb84c0b9078c2d772647c0469
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-11-20 19:43:26 +0600

    Added methods for user frindships actions

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,6 +8,56 @@ class FriendshipsController < ApplicationController
       redirect_back fallback_location: root_path
     end
   end
+
+  def accept
+    friendship = current_user.friendships.build(friend: @user, status: 'accepted')
+
+    if friendship.save
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  def reject
+    friendship = current_user.friendships.build(friend: @user, status: 'rejected')
+
+    if friendship.save
+      redirect_back fallback_location: root_path
+    end
+  end
+  
+  def cancelled
+    friendship = current_user.friendships.build(friend: @user, status: 'cancelled')
+
+    if friendship.save
+      redirect_back fallback_location: root_path
+    end
+  end
+  
+  def unfriend
+    friendship = current_user.friendships.build(friend: @user, status: 'unfriended')
+
+    if friendship.save
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  def block
+    friendship = current_user.friendships.build(friend: @user, status: 'blocked')
+
+    if friendship.save
+      redirect_back fallback_location: root_path
+    end
+  end
+
+
+  def unblock
+    friendship = current_user.friendships.build(friend: @user, status: 'unblocked')
+
+    if friendship.save
+      redirect_back fallback_location: root_path
+    end
+  end
+
   private
 
   def find_user
