4686ebb594ea86edccddc672fa963169a40002b0
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-11-24 18:45:35 +0600

    Added condition based on the flow of the request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,17 +2,27 @@ class FriendshipsController < ApplicationController
   before_action :authenticate_user!, :find_user
 
   def add
-    friendship = current_user.friendships.build(friend: @user, status: 'pending')
+    unless validity_for_friend_request(current_user, @user)
+      friendship = current_user.friendships.build(friend: @user, status: 'pending')
 
-    if friendship.save
+      if friendship.save
+        redirect_back fallback_location: root_path
+      end
+    else
+      flash.now[:warning] = 'Invalid request'
       redirect_back fallback_location: root_path
     end
   end
 
   def accept
-    friendship = current_user.friendships.build(friend: @user, status: 'accepted')
+    if validity_for_accept_request(current_user, @user)
+      friendship = current_user.friendships.build(friend: @user, status: 'accepted')
 
-    if friendship.save
+      if friendship.save
+        redirect_back fallback_location: root_path
+      end
+    else
+      flash.now[:warning] = 'Invalid request'
       redirect_back fallback_location: root_path
     end
   end
@@ -62,5 +72,15 @@ class FriendshipsController < ApplicationController
 
   def find_user
     @user = User.find_by(id: params[:user_id])
+  end
+
+  def validity_for_friend_request(user,friend)
+    current_status = Friendship.current_status?(user, friend)
+    current_status&.accepted? || current_status&.pending? || current_status&.blckoed?
+  end
+
+  def validity_for_accept_request(user,friend)
+    current_status = Friendship.current_status?(user, friend)
+    current_status&.pending?
   end
 end
