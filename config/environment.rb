d77538d1939fc21c668e8505f67ffbc3024687f3
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-01 17:06:35 +0600

    Configured sendgrid in both dev and prod environment.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.credentials.sendgrid[:sendgrid_user],
+  :password => Rails.application.credentials.sendgrid[:sendgrid_api_key],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
