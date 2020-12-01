699059f13327f32b6ae5faed8ebd985d544cd6af
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-01 19:02:55 +0600

    Added sample view for subscription plan

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,37 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(
+user1 = User.new(
   first_name: 'Shakhawat',
   last_name: 'Hossaim',
   username: 'shshamim63',
   email: 'shshamim63@gmail.com',
   password: '123456'
 )
+user1.skip_confirmation!
+user1.save!
 
-User.create(
+user2 = User.new(
   first_name: 'Bruce',
   last_name: 'Wayne',
   username: 'Batman',
   email: 'batman@gmail.com',
   password: '123456'
 )
+user2.skip_confirmation!
+user2.save!
 
-User.create(
+user3 = User.new(
   first_name: 'Dick',
   last_name: 'Grayson',
   username: 'Robin',
   email: 'robin@gmail.com',
   password: '123456'
 )
+user3.skip_confirmation!
+user3.save!
 
-User.create(
+user4 = User.new(
   first_name: 'Ben',
   last_name: 'Affleck',
   username: 'Suicide Squad',
   email: 'suicide.squad@gmail.com',
   password: '123456'
 )
+user4.skip_confirmation!
+user4.save!
 
 Friendship.create(
   user: User.first,
