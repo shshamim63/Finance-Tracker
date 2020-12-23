656b3b2ac169178701cc77980be6eb2636bb70ae
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-23 22:30:33 +0600

    Added a link in sidebar to chatroom, included routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   get 'search_users', to: "users#search"
   get 'friends', to: 'users#friends'
+  mount ActionCable.server, at: '/cable'
   resources 'messages', only: [:create]
   resources :users, only: [:index, :show] do
     resources :friendships, only: [] do
