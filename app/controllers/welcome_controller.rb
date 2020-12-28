e1921bc15ee22d59db97b5eeeec7287f6ac0296e
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-28 18:23:36 +0600

    Improved root page view and fixed single item news issue

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :about]
 
   def index
-    require 'json'
-
     @plans = Plan.all
     ids = UserStock.most_added_stocks
     stocks_ticker = Stock.selected_stock_names(ids)
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,11 @@
 class Stock < ApplicationRecord
   has_many :user_stoks
   has_many :users, through: :user_stocks
+  
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
+
   def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
@@ -18,10 +20,15 @@ class Stock < ApplicationRecord
   end
 
   def self.popular_ticker_news(ticker_symbols)
-    begin
-      recent_stocks = StockQuote::Stock.news(ticker_symbols, 2)
-    rescue Exception => e
-      recent_stocks = nil
+    news = []
+    ticker_symbols.each do |ticker|
+      begin
+        recent_stock = StockQuote::Stock.news(ticker, 2)
+        news.push(recent_stock)
+      rescue Exception => e
+        recent_stocks = nil
+      end
     end
+    news
   end
 end
