8edd4013817e2a3af1b902bc4deca586a15242a9
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-28 18:59:26 +0600

    Added factory for user and added tests for validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name { 'Pepe' }
+    last_name { 'Bas' }
+    username { 'PepeBas' }
+    email { Faker::Internet.email }
+    plan { create(:plan) }
+    password { 'foobar' }
+    password_confirmation { 'foobar' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User's validity" do
+    context "When user provides all the required info" do
+      let!(:user) { build(:user) }
+      it 'Valid user' do
+        expect(user).to be_valid
+      end
+    end
+    context "When user does not provide required info" do
+      let!(:user) { build(:user) }
+      it 'Valid user' do
+        user.email = ""
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
