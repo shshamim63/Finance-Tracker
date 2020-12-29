fffb729d140ba6462ec411d7da71459d4c549339
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-29 12:27:28 +0600

    Added plan model test

--- spec/factories/stocks.rb
+++ spec/factories/stocks.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :stock do
     ticker { "MyString" }
     name { "mystring" }
-    last_price { "9.99" }
+    last_price { 9.99 }
   end
 end
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validates plan" do
+
+    context "when fields contain all required info" do
+      let(:plan) { build(:plan) }
+      it "When plan has name, price" do
+        expect(plan).to be_valid
+      end
+    end
+
+    context "when fields do not contain all required info" do
+      let(:plan) { build(:plan) }
+      it "plan name is nil" do
+        plan.name = ""
+        expect(plan).not_to be_valid
+      end
+
+      it "plan price is nil" do
+        plan.price = nil
+        expect(plan).not_to be_valid
+      end
+    end
+  end
 end
